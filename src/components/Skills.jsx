33a070f311492d324c5e210d581df9a0a395161a
charliealpha094 <carlosamaral94@gmail.com>
2022-05-20 18:38:53 +0100

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,9 +10,29 @@ import python from "./../accessories/python.png";
 import reactjs from "./../accessories/reactjs.png";
 import ubuntu from "./../accessories/ubuntu.jpg";
 import javascript from "./../accessories/Unofficial_JavaScript_logo_2.svg.png";
+import html from "./../accessories/html.png";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div name="skills" className="bg-[#ecf0f3] text-black">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline  border-b-4">Skills</p>
+          <h2 className="py-4">
+            My knowledge and the technologies I've recently been working on...
+          </h2>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#bfc9c9] hover:scale-110 duration-300">
+            <img className="w-20 mx-auto" src={html} alt="html icon" />
+            <p className="my-3">HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
