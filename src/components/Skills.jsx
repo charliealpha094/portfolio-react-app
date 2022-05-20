8726d0c441e428dfc023b58b02c91f982e249ae4
charliealpha094 <carlosamaral94@gmail.com>
2022-05-20 16:46:54 +0100

    update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+import CSS from "./../accessories/CSS3.png";
+import github from "./../accessories/github.png";
+import linux from "./../accessories/linux.png";
+import mongodb from "./../accessories/mongodb-logo.png";
+import mysql from "./../accessories/mysql.png";
+import nodejs from "./../accessories/nodejs.png";
+import python from "./../accessories/python.png";
+import reactjs from "./../accessories/reactjs.png";
+import ubuntu from "./../accessories/ubuntu.jpg";
+import javascript from "./../accessories/Unofficial_JavaScript_logo_2.svg.png";
+
 const Skills = () => {
   return <div>Skills</div>;
 };
