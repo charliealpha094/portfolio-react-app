8aa6ea67b347491020c8825a9434100b022cb726
charliealpha094 <carlosamaral94@gmail.com>
2022-05-21 21:15:44 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,8 @@ const Contact = () => {
     >
       <form action="" className="flex flex-col max-w-[600px] w-full ">
         <div className="pb-8">
-          <p>Contact</p>
+          <p className="text-4xl font-bold inline border-b-4">Contact</p>
+          <p className=""></p>
         </div>
       </form>
     </div>
