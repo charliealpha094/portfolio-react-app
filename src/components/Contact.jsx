513c2abd3b3dae8488f4fd5422b3ff6fd95f2f98
charliealpha094 <carlosamaral94@gmail.com>
2022-05-21 18:28:44 +0100

    initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div name="contact" className="w-full h-screen bg-[#ecf0f3]">
+      <form action=""></form>
+    </div>
+  );
+};
+
+export default Contact;
