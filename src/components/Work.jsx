ae56e7f2b1b5841377dd258bcd6a2769e7c2e1da
charliealpha094 <carlosamaral94@gmail.com>
2022-05-21 11:53:43 +0100

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 
+import Defensive from "./../accessories/Defensive.png";
+
 const Work = () => {
   return (
-    <div className="w-full md:h-screen">
-      <div>
-        <div>
-          <p>Work</p>
-          <p>Some of my most recent works!!</p>
+    <div name="work" className="w-full md:h-screen text-gray-700 bg-[#fff]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4">Work</p>
+          <p className="py-6">Some of my most recent works!!</p>
         </div>
 
-        <div>
-          <div>
+        <div className="grid sm:grid-cols-2 m:grid-cols-3 gap-4">
+          <div className="shadow-lg shadow-[#ecf0f3] group container rounded-md flex justify-center items-center mx-auto content-div">
+            {/* Hover Effects */}
             <div>
               <span></span>
               <div>
+                <a href="/">
+                  <button></button>
+                </a>
                 <a href="/">
                   <button></button>
                 </a>
