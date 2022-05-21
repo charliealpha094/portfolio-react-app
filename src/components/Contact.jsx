2434e27a84e87d3ba0f277da2124e3834b586413
charliealpha094 <carlosamaral94@gmail.com>
2022-05-21 18:42:47 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div name="contact" className="w-full h-screen bg-[#ecf0f3]">
-      <form action=""></form>
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#ecf0f3] flex justify-center items-center p-4"
+    >
+      <form action="" className="flex flex-col max-w-[600px] w-full ">
+        <div className="pb-8">
+          <p>Contact</p>
+        </div>
+      </form>
     </div>
   );
 };
