4cec944737204a6d85c19923aba1a74e1a683b07
charliealpha094 <carlosamaral94@gmail.com>
2022-05-22 16:58:25 +0100

    update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -35,7 +35,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/charliealpha094/defensive-react">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -54,12 +54,12 @@ const Work = () => {
               </span>
 
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://my-simple-weather.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/charliealpha094/weather-react">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
