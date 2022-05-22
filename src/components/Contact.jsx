aeea53cf5fda30750d68968caee73e9c3985a86a
charliealpha094 <carlosamaral94@gmail.com>
2022-05-22 12:09:18 +0100

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,26 @@ const Contact = () => {
       <form action="" className="flex flex-col max-w-[600px] w-full ">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4">Contact</p>
-          <p className=""></p>
+          <p className="py-4">
+            To contact me submit the form below or simply send me an email
+            directly to -{" "}
+            <span className="text-[#0c4e83]"> carlosamaral94@gmail.com</span>
+          </p>
         </div>
+        <input
+          className="bg-[#fff]"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="bg-[#fff] my-4 p-2"
+          type="text"
+          placeholder="Email"
+          name="name"
+        />
+        <textarea name="message" cols="30" rows="9"></textarea>
+        <button className="">Submit</button>
       </form>
     </div>
   );
